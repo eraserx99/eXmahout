142cc749135018b729009c9fd31994b016e1ea15
Steve Chien <steve@productlab.com>
2013-01-12 10:58:16 -0500

    changes

--- src/main/java/com/esoteric/mahout/sample/ClusterDumper.java
+++ src/main/java/com/esoteric/mahout/sample/ClusterDumper.java
@@ -17,51 +17,51 @@ import org.apache.mahout.math.NamedVector;
 
 public class ClusterDumper {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			BufferedWriter bw;
-			Configuration conf = new Configuration();
-			FileSystem fs = FileSystem.get(conf);
-			File pointsFolder = new File(args[0]);
-			File files[] = pointsFolder.listFiles();
-			bw = new BufferedWriter(new FileWriter(new File(args[1])));
-			HashMap<String, Integer> clusterIds;
-			clusterIds = new HashMap<String, Integer>(5000);
-			for (File file : files) {
-				if (file.getName().indexOf("part-m") < 0)
-					continue;
-				String aPath = file.getAbsolutePath();
-				SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path(aPath), conf);
-				IntWritable key = new IntWritable();
-				WeightedVectorWritable value = new WeightedVectorWritable();
-				while (reader.next(key, value)) {
-					NamedVector vector = (NamedVector) value.getVector();
-					String vectorName = vector.getName();
-					bw.write(vectorName + "\t" + key.toString() + "\t" + vector.toString() + "\n");
-					if (clusterIds.containsKey(key.toString())) {
-						clusterIds.put(key.toString(),
-								clusterIds.get(key.toString()) + 1);
-					} else
-						clusterIds.put(key.toString(), 1);
-				}
-				bw.flush();
-				reader.close();
-			}
-			bw.flush();
-			bw.close();
-			bw = new BufferedWriter(new FileWriter(new File(args[2])));
-			Set<String> keys = clusterIds.keySet();
-			for (String key : keys) {
-				bw.write(key + " " + clusterIds.get(key) + "\n");
-			}
-			bw.flush();
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    try {
+      BufferedWriter bw;
+      Configuration conf = new Configuration();
+      FileSystem fs = FileSystem.get(conf);
+      File pointsFolder = new File(args[0]);
+      File files[] = pointsFolder.listFiles();
+      bw = new BufferedWriter(new FileWriter(new File(args[1])));
+      HashMap<String, Integer> clusterIds;
+      clusterIds = new HashMap<String, Integer>(5000);
+      for (File file : files) {
+        if (file.getName().indexOf("part-m") < 0)
+          continue;
+        String aPath = file.getAbsolutePath();
+        SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path(aPath), conf);
+        IntWritable key = new IntWritable();
+        WeightedVectorWritable value = new WeightedVectorWritable();
+        while (reader.next(key, value)) {
+          NamedVector vector = (NamedVector) value.getVector();
+          String vectorName = vector.getName();
+          bw.write(vectorName + "\t" + key.toString() + "\t" + vector.toString() + "\n");
+          if (clusterIds.containsKey(key.toString())) {
+            clusterIds.put(key.toString(),
+              clusterIds.get(key.toString()) + 1);
+          } else
+          clusterIds.put(key.toString(), 1);
+        }
+        bw.flush();
+        reader.close();
+      }
+      bw.flush();
+      bw.close();
+      bw = new BufferedWriter(new FileWriter(new File(args[2])));
+      Set<String> keys = clusterIds.keySet();
+      for (String key : keys) {
+        bw.write(key + " " + clusterIds.get(key) + "\n");
+      }
+      bw.flush();
+      bw.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
